e5a99aae947a6287406bcb226cc5794070f5296b
Isamu Ito <isamsito@gmail.com>
2021-02-11 03:59:31 +0000

    working

--- fetcher.js
+++ fetcher.js
@@ -1,16 +1,39 @@
 const request = require('request');
 const fs = require('fs');
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const args = process.argv.slice(2);
 
 const website = args[0];
 const dir = args[1];
 
+
+
 request(website, (error, response, body) => {
   if (error) throw error;
-  fs.writeFile(dir, body, (err) => {
-    if (err) throw err;
-    console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${dir}`);
+  fs.access(dir,fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.writeFile(dir, body, (err) => {
+        if (err) throw err;
+        console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${dir}`);
+      });
+    } else {
+      rl.question('The file already exists would you like to overwrite y/n?', (answer) => {
+        if (answer === 'y') {
+          fs.writeFile(dir, body, (err) => {
+            if (err) throw err;
+            console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${dir}`);
+            });
+        } else {
+          console.log('Try a different directory then');
+        }
+        rl.close();
+      });
+    }
   });
-
+  rl.close();
 });
+    // 
